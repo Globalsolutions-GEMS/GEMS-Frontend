0e13d09e55c047f194f750d46b41ba65e8f72695
Shweta <shwetakatore2@gmail.com>
2024-03-21 18:33:11 +0530

    certificate

---
+++ app/(dashboard)/pages/academic/studentreport/certificate/studenticard/page.js
@@ -0,0 +1,226 @@
+"use client";
+// import node module libraries
+import { Alert, Badge, Breadcrumb, Container } from "react-bootstrap";
+import { Col, Row, Form, Card, Button } from "react-bootstrap";
+
+// import widget as custom components
+import { PageHeading } from "widgets";
+
+// import sub components
+import useMounted from "hooks/useMounted";
+
+const StudentICard = () => {
+  const hasMounted = useMounted();
+
+  return (
+    <Container fluid className="p-6">
+      {/* Page Heading */}
+      <PageHeading heading="Student I Card " />
+      <Breadcrumb>
+        <Breadcrumb.Item href="#">Academic</Breadcrumb.Item>
+        <Breadcrumb.Item href="#">Student Report</Breadcrumb.Item>
+        <Breadcrumb.Item href="#">Certificate</Breadcrumb.Item>
+        <Breadcrumb.Item active>Student I Card</Breadcrumb.Item>
+      </Breadcrumb>
+
+      <Row className="mb-8">
+        <Col xl={12} lg={8} md={12} xs={12}>
+          <Card>
+            <Card.Body>
+              <div>
+                {hasMounted && (
+                  <Form>
+                    <Row className="mb-3">
+                      <Form.Label
+                        className="col-sm-2 col-form-label form-label"
+                        htmlFor="fullName"
+                      >
+                        Academic Year<span className="text-danger">*</span>
+                      </Form.Label>
+                      <Col sm={4} className="mb-3 mb-lg-0">
+                        <Form.Select
+                          type="text"
+                          placeholder="Please Enter "
+                          id="Academic Year"
+                          required
+                        />
+                      </Col>
+                    </Row>
+                    <Row className="mb-3">
+                      <Form.Label
+                        className="col-sm-2 col-form-label form-label"
+                        htmlFor="fullName"
+                      >
+                        Programme<span className="text-danger">*</span>
+                      </Form.Label>
+                      <Col sm={4} className="mb-3 mb-lg-0">
+                        <Form.Select
+                          type="text"
+                          placeholder="Please Select Course"
+                          id="course"
+                          required
+                        />
+                      </Col>
+                    </Row>
+                    <Row className="mb-3">
+                      <Form.Label
+                        className="col-sm-2 col-form-label form-label"
+                        htmlFor="fullName"
+                      >
+                        Course
+                      </Form.Label>
+                      <Col sm={4} className="mb-3 mb-lg-0">
+                        <Form.Select
+                          type="text"
+                          placeholder="Please Select Course"
+                          id="course"
+                          required
+                        />
+                      </Col>
+                    </Row>
+                    <Row className="mb-3">
+                      <Form.Label
+                        className="col-sm-2 col-form-label form-label"
+                        htmlFor="fullName"
+                      >
+                        Medium
+                      </Form.Label>
+                      <Col sm={4} className="mb-3 mb-lg-0">
+                        <Form.Select
+                          type="text"
+                          placeholder="Please Select Course"
+                          id="course"
+                          required
+                        />
+                      </Col>
+                    </Row>
+                    <Row className="mb-3">
+                      <Form.Label
+                        className="col-sm-2 col-form-label form-label"
+                        htmlFor="fullName"
+                      >
+                        Section
+                      </Form.Label>
+                      <Col sm={4} className="mb-3 mb-lg-0">
+                        <Form.Select
+                          type="text"
+                          placeholder="Please Select Course"
+                          id="course"
+                          required
+                        />
+                      </Col>
+                    </Row>
+                    <Row className="mb-3">
+                      <Form.Label
+                        className="col-sm-2 col-form-label form-label"
+                        htmlFor="fullName"
+                      >
+                        Order By
+                      </Form.Label>
+                      <Col sm={4} className="mb-3 mb-lg-0">
+                        <Form.Select
+                          type="text"
+                          placeholder="Please Select Course"
+                          id="course"
+                          required
+                        />
+                      </Col>
+                    </Row>
+                    <Row className="mb-3">
+                      <Form.Label
+                        className="col-sm-2 col-form-label form-label"
+                        htmlFor="fullName"
+                      >
+                        From Date
+                      </Form.Label>
+                      <Col sm={4} className="mb-3 mb-lg-0">
+                        <Form.Control
+                          type="date"
+                          placeholder="Please Enter From Date"
+                          id="Academic Year"
+                          required
+                        />
+                      </Col>
+                    </Row>
+                    <Row className="mb-3">
+                      <Form.Label
+                        className="col-sm-2 col-form-label form-label"
+                        htmlFor="fullName"
+                      >
+                        To Date
+                      </Form.Label>
+                      <Col sm={4} className="mb-3 mb-lg-0">
+                        <Form.Control
+                          type="date"
+                          placeholder="Please Enter To Date"
+                          id="course"
+                          required
+                        />
+                      </Col>
+                    </Row>
+
+                    <Row className="mb-3">
+                      <Form.Label className="col-sm-2 col-form-label form-label"></Form.Label>
+
+                      <Col sm={2} className="mt-2">
+                        <Form.Check
+                          label="Front/Back"
+                          type="radio"
+                          name="radio"
+                        />
+                      </Col>
+                      <Col sm={2} className="mt-2">
+                        <Form.Check
+                          label="Front Print"
+                          type="radio"
+                          name="radio"
+                        />
+                      </Col>
+                      <Col sm={2} className="mt-2">
+                        <Form.Check
+                          label="Back Print"
+                          type="radio"
+                          name="radio"
+                        />
+                      </Col>
+                    </Row>
+                    <Row className="mb-3">
+                      <Form.Label className="col-sm-2 col-form-label form-label">
+                        Export To Excel
+                      </Form.Label>
+                      <Form.Check
+                        className="col-sm-8 col-form-label form-label"
+                        type="checkbox"
+                        id="active"
+                      >
+                        <Form.Check.Input type="checkbox" />
+                      </Form.Check>
+                    </Row>
+                    <Row>
+                      <Col className="d-flex align-items-center justify-content-center">
+                        <Button variant="primary" type="submit">
+                          Show
+                        </Button>
+
+                        <Button
+                          variant="secondary"
+                          type=""
+                          style={{ marginLeft: "10px" }}
+                        >
+                          Cancel
+                        </Button>
+                      </Col>
+                      <Col></Col>
+                    </Row>
+                  </Form>
+                )}
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default StudentICard;
