901e7d3802f14dde0a92f2c4dd2d1eea06472e0f
PraphullaSawant <praphullasawant123@gmail.com>
2024-04-04 19:23:45 +0530

    Redeploy

--- app/(dashboard)/studentdashboard/page.js
+++ app/(dashboard)/studentdashboard/page.js
@@ -1,23 +1,15 @@
 "use client";
 // import node module libraries
-import { Fragment, useState } from "react";
+import { Fragment, } from "react";
 import * as React from "react";
-import Link from "next/link";
-import { Container, Col, Row, Card } from "react-bootstrap";
+import { Container, Col, Row } from "react-bootstrap";
 
 // import widget/custom components
-import { StatRightTopIcon } from "widgets";
 import StudentChart from "../../../sub-components/studentdashboard/StudentChart";
 import Notifications from "../../../sub-components/studentdashboard/Notifications";
 import Attendance from "../../../sub-components/studentdashboard/Attendance";
 import TodaysEvents from "../../../sub-components/studentdashboard/TodaysEvents"
 
-// import required data files
-import ProjectsStatsData from "data/dashboard/ProjectsStatsData";
-
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
 
 const Home = () => {
   return (
